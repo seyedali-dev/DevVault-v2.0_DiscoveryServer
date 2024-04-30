a38b1a9b06855952293c2aaeecccac26b70d2531
SayedAli <sayedxali7@gmail.com>
2024-04-30 21:06:33 +0330

    discovery server initial commit

---
+++ src/test/java/com/seyed/ali/discoveryserver/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seyed.ali.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
